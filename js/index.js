1ae6d8f3921c0c3786bb69b6ec9d479d491b32f3
joimar755 <joimarjose19@gmail.com>
2023-03-21 15:45:12 -0500

    aaaa

--- js/index.js
+++ js/index.js
@@ -3,3 +3,7 @@ $(".menu-toggle-btn").click(function (e) {
    $(".navigation-menu").toggleClass("active");
     
 });
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0);
+})
