6198485ccce292ac659d302b0c05eedd26e52e86
joimar755 <joimarjose19@gmail.com>
2023-03-31 01:16:15 -0500

    a

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
 $(".menu-toggle-btn").click(function (e) { 
    $(this).toggleClass("fa-times");
    $(".navigation-menu").toggleClass("active");
-    
 });
 window.addEventListener("scroll", function() {
     var header = document.querySelector("header");
     header.classList.toggle("sticky",window.scrollY > 0);
+})
+//Acordeon
+let abrir = document.getElementById("show")
+abrir.addEventListener("click", function() {
+    let element = document.getElementById('to-show');
+    if (element.className === 'hide') {
+      element.className = 'show';
+    } else {
+      element.className = 'hide';
+    }
 })
